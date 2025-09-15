d8ec2e4725a4a287a7c0ae979fea34f194aef06e
fr3zill@ <34129644+frezilla@users.noreply.github.com>
2025-09-15 11:14:42 +0000

    Ajout de la méthode de mise en forme du résultat

--- 2015/day_10/main.py
+++ 2015/day_10/main.py
@@ -1,3 +1,16 @@
+def format_result(value):
+    result = ""
+    character = value[0]
+    value_length = len(value)
+    nb_groups = value_length // 9
+    remain = value_length % 9
+    for i in range(0, nb_groups):
+        result += '9' + character
+    if remain > 0:
+        result += str(remain) + character
+    return result
+
+
 def look_and_say(val):
     result = ''
     sub_string = ""
@@ -6,22 +19,20 @@ def look_and_say(val):
         if current_char == previous_char:
             sub_string += current_char
         else:
-            if len(sub_string) > 0:
-                result += str(len(sub_string)) + sub_string[0]
+            sub_string_length = len(sub_string)
+            if sub_string_length > 0:
+                result += format_result(sub_string)
             sub_string = current_char
             previous_char = current_char
-    result += str(len(sub_string)) + sub_string[0]
+    result += format_result(sub_string)
     return result
 
 print("--- 10: Elves Look, Elves Say ---")
-
 puzzle = input("Puzzle initial : ")
-
 value = puzzle
 for i in range(0, 40):
     value = look_and_say(value)
 print(f"The length of the result is {len(value)} (40 times)")
-
 value = puzzle
 for i in range(0, 50):
     value = look_and_say(value)
