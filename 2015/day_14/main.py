e8331c41a6d81b972f6ead9767e21eec98911c49
fr3zill@ <34129644+frezilla@users.noreply.github.com>
2025-10-13 14:54:24 +0000

    Finalisation de la 1ère partie du challenge

--- 2015/day_14/main.py
+++ 2015/day_14/main.py
@@ -15,9 +15,16 @@ class Reinder:
         return distance
 
 print("--- Day 14: Reindeer Olympics ---")
-
-comet = Reinder("Comet", 14, 10, 127)
-dancer = Reinder("Dancer", 16, 11, 162)
-
-print(comet.run(1000))
-print(dancer.run(1000))
+TIME = 2503
+inputFile = input("Input file name : ")
+file = open(inputFile, 'r')
+reinders = []
+for line in file:
+    datas = line.strip().split()
+    reinders.append(Reinder(datas[0], int(datas[3]), int(datas[6]), int(datas[13])))
+max_distance = 0
+for reinder in reinders:
+    distance = reinder.run(TIME)
+    if distance > max_distance:
+        max_distance = distance
+print(f"Distance maximale parcourue par le vainqueur = {max_distance}")
